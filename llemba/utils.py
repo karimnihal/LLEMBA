75d9f0f50fe5b17fbdc16904d2fa700182dab8e7
karimnihal <karimnihal2@gmail.com>
2024-11-09 12:43:39 -0800

    converted utils and prompt

--- llemba/utils.py
+++ llemba/utils.py
@@ -1,1 +1,29 @@
-from llemba.gpt_api import TogetherAPI
+import ipdb
+import pandas as pd
+import diskcache as dc
+
+from llemba.together_api import TogetherAPI
+from llemba.llemba_mqm_utils import TEMPLATE_LLEMBA_MQM, apply_template, parse_mqm_answer
+from llemba.prompt import prompts, validate_number
+
+
+def get_llemba_scores(source, hypothesis, source_lang, target_lang, method, model):
+    df = pd.DataFrame({'source_seg': source, 'target_seg': hypothesis})
+    df['source_lang'] = source_lang
+    df['target_lang'] = target_lang
+
+    cache = dc.Cache(f'cache/{model}_{method}', expire=None, size_limit=int(10e10), cull_limit=0, eviction_policy='none')
+    togetherapi = TogetherAPI()
+
+    if method == "LLEMBA-MQM":
+        df["prompt"] = df.apply(lambda x: apply_template(TEMPLATE_LLEMBA_MQM, x), axis=1)
+        parse_answer = lambda x: parse_mqm_answer(x, list_mqm_errors=False, full_desc=True)
+        answers = togetherapi.bulk_request(df, model, parse_answer, cache=cache, max_tokens=500)
+    elif method in ["LLEMBA-DA", "LLEMBA-DA_ref", "LLEMBA-SQM", "LLEMBA-SQM_ref", "LLEMBA-stars", "LLEMBA-stars_ref", "LLEMBA-classes", "LLEMBA-classes_ref"]:
+        df["prompt"] = df.apply(lambda x: apply_template(prompts[method]['prompt'], x), axis=1)
+        parse_answer = prompts[method]["validate_answer"]
+        answers = togetherapi.bulk_request(df, model, parse_answer, cache=cache, max_tokens=500)
+    else:
+        raise Exception(f"Method {method} not supported.")
+
+    return list(pd.DataFrame(answers)['answer'])
